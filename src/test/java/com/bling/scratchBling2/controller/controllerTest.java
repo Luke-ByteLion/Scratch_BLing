caf56dc9a8da8acc93d373ea9571f734541fc219
luke-ByteLion <luke.maclean@bytelion.com>
2021-06-21 14:50:14 -0400

    acceptance tests

---
+++ src/test/java/com/bling/scratchBling2/controller/controllerTest.java
@@ -0,0 +1,15 @@
+package com.bling.scratchBling2.controller;
+
+import com.bling.scratchBling2.model.ScratcherModel;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class controllerTest {
+
+    @Test
+    void getScratchers() {
+        ScratcherModel scratcher = new ScratcherModel();
+        //assertEquals("");
+    }
+}
